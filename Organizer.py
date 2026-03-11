4fb6fa6faf3b3e96b5e1d4c5eb5a05239cf7f5ce
Miguel <miguelbritohudson3345@gmail.com>
2026-03-11 18:25:00 -0300

    Initial commit

---
+++ Organizer.py
@@ -0,0 +1,29 @@
+import os
+import shutil
+
+Inicio = input("Digite o caminho da pasta que deseja organizar: ")
+pasta = "caminho/da/pasta" #troque para o diretório desejado
+
+tipos_arquivos = {
+       "Imagens": [".png", ".jpg", ".jpeg"],
+       "PDF": [".pdf", ".PDF"],
+       "Musicas": [".mp3", ".m4a"],
+       "Video": [".avi", ".mov", ".mp4"],
+       "Documentos": [".docx", ".txt"],
+}
+
+for arquivo in os.listdir(pasta):
+    caminho_arquivo = os.path.join(pasta, arquivo)
+
+    if os.path.isfile(caminho_arquivo):
+        for pasta_destino, extensoes in tipos_arquivos.items():
+            if arquivo.lower().endswith(tuple(extensoes)):
+
+                nova_pasta = os.path.join(pasta, pasta_destino)
+
+                if not os.path.exists(nova_pasta):
+                    os.makedirs(nova_pasta)
+
+                shutil.move(caminho_arquivo, os.path.join(nova_pasta, arquivo))
+
+print("Arquivos organizados com sucesso!")
---
+++ README.md
@@ -0,0 +1,21 @@
+File Organizer Python
+
+Projeto simples em Python que organiza automaticamente arquivos de uma pasta com base na extensão.
+
+Tecnologias
+- Python
+
+Funcionalidades
+- Organiza imagens
+- Organiza documentos
+- Organiza vídeos
+- Organiza músicas
+
+Como executar
+
+1. Clone o repositório
+2. Execute o arquivo:
+
+python organizer.py
+
+3. Digite o caminho da pasta que deseja organizar.
